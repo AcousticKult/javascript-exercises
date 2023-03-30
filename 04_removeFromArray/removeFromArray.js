68afce5c767c2c813af7d8abc82753b4a4365f3f
AcousticKult <acoustickult@outlook.com>
2023-03-30 14:57:59 +0000

    Update exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, item) {
+    let finalArray = [];
+    
+    // cycle through the array
+    for (let i = 0; i < myArray; i++) {
+        // identify which arrayItem is same as item
+        if (myArray[i] !== item) {
+            finalArray.push(myArray[i]);
+        }
+    }
 
+    return finalArray;
 };
 
 // Do not edit below this line
