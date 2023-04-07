b40e2e4c32accef78bb9c21508aea0f58be3667e
AcousticKult <acoustickult@outlook.com>
2023-04-07 06:29:16 +0000

    Update 04 solution comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
 const removeFromArray = function(myArray) {
     // USE FOR LOOP OR FOR...OF TO COMPARE myArray and arguments?
-    // if (arguments[0, ...spread? ...rest?]) === any of myArray.elements) {
-        // remove myArray.element that is same as arguments[n]
-        // myArray.splice(arguments, 1)    
+    // for (element of myArray) {
+        // if (arguments[0, ...spread? ...rest?]) === myArray.element) {
+            // remove myArray.element that is same as arguments[n]
+            // myArray.splice(arguments, 1)    
+        // }
     // }
     return myArray;
 };
