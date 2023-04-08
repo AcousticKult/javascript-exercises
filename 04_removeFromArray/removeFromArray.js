c31429eb7c73c79cfe9992d36f41df976c7d4e40
AcousticKult <acoustickult@outlook.com>
2023-04-08 00:23:12 +0000

    Update pass solution for loop 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
 const removeFromArray = function(myArray, ...moreArgs) {
     // USE FOR LOOP OR FOR...OF TO COMPARE myArray and arguments?
     for (let x of myArray) {
-        if (x === moreArgs[0]) {
-            // remove myArray[x] that is same as moreArgs[n]
-            myArray.splice(myArray.indexOf(x), 1)    
+        for (let n of moreArgs) {
+            if (x === n) {
+                myArray.splice(myArray.indexOf(x), 1);
+            }
         }
     }
     return myArray;
