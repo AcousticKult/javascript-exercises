6e4797caf25e95b41bc57d83c839df302490c695
AcousticKult <acoustickult@outlook.com>
2023-04-20 01:33:49 +0000

    Investigate remove all in 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,13 @@
 const removeFromArray = function(myArray, ...moreArgs) {
-    for (let x of myArray) {
+    for (let i = 0; i <= myArray.length; i++) {
+    // for (let i of myArray) {
         for (let n of moreArgs) {
-            if (n === x) {
-                myArray.splice(myArray.indexOf(x), 1);
+            if (n === i) {
+                myArray.splice(myArray.indexOf(i), 1);
             }
             continue;
-        // continue;
         }
+        continue;
     }
     return myArray;
 };
