2baaa45fde572a6e0e51e5b8c6654c5f74e3e281
AcousticKult <acoustickult@outlook.com>
2023-04-23 16:13:21 +0000

    Add complete exercises before 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,13 @@
 const removeFromArray = function(myArray, ...moreArgs) {
-    for (let i = 0; i <= myArray.length; i++) {
-    // for (let i of myArray) {
-        for (let n of moreArgs) {
-            if (n === i) {
-                myArray.splice(myArray.indexOf(i), 1);
-            }
-            continue;
+    const newArray = [];
+
+    myArray.forEach(item => {
+        if (!moreArgs.includes(item)) {
+            newArray.push(item);
         }
-        continue;
-    }
-    return myArray;
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
