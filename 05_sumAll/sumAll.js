cef732b0ed141d2381dd69e9380afc4cfb871a30
AcousticKult <45168150+AcousticKult@users.noreply.github.com>
2023-05-03 10:43:56 -0400

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let finalSum = 0;
 
+  if ((a < 0) || (b < 0)) {
+    return "ERROR";
+  } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+      return "ERROR";
+  } else if (a > b) {
+      for (let i = a; i >= b; i--) {
+        finalSum += i;
+      }
+  } else {
+      for (let i = a; i <= b; i++) {
+        finalSum += i;
+      }  
+  }
+
+  return finalSum;
 };
 
 // Do not edit below this line
